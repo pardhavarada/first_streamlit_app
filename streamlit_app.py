8220a7e11c1fad73dacdf56685602d837a2765d5
pardhavarada <72842472+pardhavarada@users.noreply.github.com>
2023-05-13 20:19:47 +0800

    Update streamlit_app.py
    output the response in JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit
-import pandas
-import requests
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -17,8 +15,13 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+import pandas
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
